4450e653561bf58f681caed6bd7354677d828241
Yoshi Spendiff <Yoshi@DevOps001.local>
2015-03-11 09:50:13 -0700

    More fixes for apache 2.4

--- apache24/attributes/apache.rb
+++ apache24/attributes/apache.rb
@@ -42,7 +42,7 @@ when 'rhel'
   default[:apache][:binary]        = '/usr/sbin/httpd'
   default[:apache][:icondir]       = '/var/www/icons/'
   default[:apache][:init_script]   = '/etc/init.d/httpd'
-  default[:apache][:version]       = '2.2'
+  default[:apache][:version]       = '2.4'
   default[:apache][:conf_available_dir]  = "#{node[:apache][:dir]}/conf.d"
   default[:apache][:conf_enabled_dir]    = "#{node[:apache][:dir]}/conf.d"
   default[:apache][:pid_file]      = '/var/run/httpd/httpd.pid'
