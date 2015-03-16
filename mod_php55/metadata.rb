513ca17d3b08798f46f361ebe9192aabb659850f
Yoshi Spendiff <Yoshi@DevOps001.local>
2015-03-16 12:32:06 -0700

    Updated mod_php55 metadata file

--- mod_php55/metadata.rb
+++ mod_php55/metadata.rb
@@ -1,7 +1,5 @@
-name        "mod_php55_apache24"
+name        "mod_php55"
 description "Installs/Configures Apache with mod_php55"
-maintainer  "AWS OpsWorks"
-license     "Apache 2.0"
-version     "1.0.0"
+version     "0.1.0"
 
-depends "apache24"
+depends "apache2"
