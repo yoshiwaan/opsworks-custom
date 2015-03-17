2369194bac4a41ee47ccf43e264eaf3ac411389b
Yoshi Spendiff <Yoshi@DevOps001.local>
2015-03-17 11:57:38 -0700

    Using forked version of varnish cookbook to fix bugs

--- indochino-varnish/metadata.rb
+++ indochino-varnish/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures indochino-varnish'
 long_description 'Installs/Configures indochino-varnish'
-version          '0.1.13'
+version          '0.1.14'
 
-depends 'varnish', '< 1.2.0'
+depends 'varnish', '= 1.0.3'
