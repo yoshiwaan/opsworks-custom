5cc2c35a7dd9fcddf4513c848a7e9f6c8796149a
Yoshi Spendiff <Yoshi@DevOps001.local>
2015-03-17 11:14:19 -0700

    bugfix

--- indochino-varnish/metadata.rb
+++ indochino-varnish/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures indochino-varnish'
 long_description 'Installs/Configures indochino-varnish'
-version          '0.1.10'
+version          '0.1.12'
 
 depends 'varnish', '< 1.2.0'
