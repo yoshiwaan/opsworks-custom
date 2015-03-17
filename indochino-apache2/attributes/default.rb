a4c1ac6d50c11dbbba7dd9b9284002167c6db4bc
Yoshi Spendiff <Yoshi@DevOps001.local>
2015-03-16 17:13:44 -0700

    Added indochino-apache2 recipe which supports httpd24 package

---
+++ indochino-apache2/attributes/default.rb
@@ -0,0 +1,3 @@
+normal['apache']['version'] = '2.4'
+normal['apache']['package'] = 'httpd24'
+normal['apache']['service_name'] = 'httpd'
---
+++ indochino-apache2/metadata.rb
@@ -0,0 +1,9 @@
+name             'indochino-apache2'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures indochino-apache2'
+long_description 'Installs/Configures indochino-apache2'
+version          '0.1.0'
+
+depends 'apache2'
